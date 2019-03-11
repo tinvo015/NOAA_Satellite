72c445422cea1fc79211ca44dfb16174f0130b03
hurmabi <ngonathan@hotmail.com>
2019-03-11 12:49:10 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # NOAA_Satellite
-
-3/9/2019 2:56pm made some improvements, unabe to get perfect sync on demod due to inconsitent sync patterns on final tests
--- demod.m
+++ demod.m
@@ -1,7 +1,7 @@
 clear all;
 close all;
 
-[y, fs] = audioread('finaltest2.wav');
+[y, fs] = audioread('finaltest3.wav');
 y = resample(y, 16640, 16000);
 y0 = hilbert(y);
 y0i = imag(y0);
@@ -16,7 +16,7 @@ F = fdesign.lowpass('N,F3db', 6, fc, Fs_new);
 d = design(F);
 y_baseband = filtfilt(d.Numerator, 1, y);
 
-y1 = decimate(y_baseband, 4);
+y1 = decimate(y_baseband, 4, 6);
 
 % Creates sync A 
 sync_A = zeros(1, 39);
@@ -25,25 +25,42 @@ for i = 5:4:30
     sync_A(i:(i+1)) = 1;
 end
 
-y1 = y1(10000:end);
+y1 = y1(2300:end);
 
-c = xcorr(sync_A',y1);
-c = c(39:end);
-[max_v, max_i] = max(c(1:3000));
+c = conv(sync_A', y1(1:2080));
+c = c(1:2080);
+[max_v, max_i] = max(c);
+
+% imax = mod(max_i, 2080);
+% imax = 2080 - imax;
 
 image = [];
-y1 = y1(max_i:end);
+% y1 = y1(max_i:end);
 
-i = 1;
+i = max_i;
 j = 1;
 while i < length(y1) - 2100
-    i = i + 39 + 47;
+    i = i + 47;
     image(j,:) = y1(i:i + 908);
     j = j+1;
-    i = i + (2080 - (39+47));
+    i = i + (2080 - (47));
     
 end
 
 %image = uint8(image);
+figure();
+subplot(2, 2, 3);
+plot(c);
+title('cross correlation');
+subplot(2, 2, 2);
+plot(sync_A);
+title('sync A pattern');
+subplot(2, 2, 1);
+plot(y1(1:2080));
+title('one data frame length of recieved signal');
+subplot(2, 2, 4);
 imshow(image);
+
+figure();
+plot(y1);
     
